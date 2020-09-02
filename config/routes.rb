d4ab9d74f8d28b48eae937413bb79e03486cb5d4
Taylor Dorsett <taylorsunwatedemails@gmail.com>
2020-09-01 20:40:12 -0500

    fix route file

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
     get "/product" => "products#show_products"
-  end
-
-  namespace :api do
     get "/all_product" => "products#all_products"
   end
 end
