bca9a8381c137b307fd5d210ef4514cd26b2ca78
Taylor Dorsett <taylorsunwatedemails@gmail.com>
2020-09-02 20:09:23 -0500

    add better paths

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -8,4 +8,11 @@ class Api::ProductsController < ApplicationController
     @all_products = Product.all
     render "all_product.json.jb"
   end
+
+  def one_product_action
+    # http://localhost:3000/api/one_product_path?product_id=1
+    # http://localhost:3000/api/one_product_path/1
+    @product = Product.find_by(id: params["product_id"])
+    render "product.json.jb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   namespace :api do
     get "/product" => "products#show_products"
     get "/all_product" => "products#all_products"
+
+    post "/product_"
+
+    get "/one_product_path" => "products#one_product_action"
+    get "/one_product_path/:product_id" => "products#one_product_action"
   end
 end
