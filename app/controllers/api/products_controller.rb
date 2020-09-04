d9b01232d27e69e4cdf35baec5524ad531b5a3f6
Taylor Dorsett <taylorsunwatedemails@gmail.com>
2020-09-03 20:40:36 -0500

    add params

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,18 +1,23 @@
 class Api::ProductsController < ApplicationController
-  def show_products
-    @product = Product.first
-    render "product.json.jb"
+  def show
+    # http://localhost:3000/api/products/1
+    @product = Product.find_by(id: params[:id])
+    render "show.json.jb"
   end
 
-  def all_products
-    @all_products = Product.all
-    render "all_product.json.jb"
+  def index
+    @products = Product.all
+    render "index.json.jb"
   end
 
-  def one_product_action
-    # http://localhost:3000/api/one_product_path?product_id=1
-    # http://localhost:3000/api/one_product_path/1
-    @product = Product.find_by(id: params["product_id"])
-    render "product.json.jb"
+  def create
+    @product = Product.new(
+      name: params[:name],
+      price: params[:price],
+      image_url: params[:image_url],
+      description: params[:description],
+    )
+    @product.save
+    render "show.json.jb"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
-    get "/product" => "products#show_products"
-    get "/all_product" => "products#all_products"
-
-    post "/product_"
-
-    get "/one_product_path" => "products#one_product_action"
-    get "/one_product_path/:product_id" => "products#one_product_action"
+    get "/products" => "products#index"
+    post "/products" => "products#create"
+    get "/products/:id" => "products#show"
   end
 end
