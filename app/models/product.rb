3d91288fa83368a406ec12ad0ba4d182f9aa90fc
Taylor Dorsett <taylorsunwatedemails@gmail.com>
2020-09-08 18:44:10 -0500

    add validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,15 @@
 class Product < ApplicationRecord
+  validates :name, presence: true
+  validates :price, numerricality: { greater_than: 0 }
+  validates :name, length: { minimum: 2 }
+  validates :description, length: { in: 2..500 }
+  validates :id, uniqueness: true
+end
+
+def tax
+  price * 0.09
+end
+
+def total
+  price + tax
 end
