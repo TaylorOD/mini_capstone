8070f96975fbf2ac36710deb3e66bbc97f0fe6e9
Taylor Dorsett <taylorsunwatedemails@gmail.com>
2020-09-09 20:27:16 -0500

    add search params

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,13 +1,28 @@
 class Api::ProductsController < ApplicationController
+  def index
+    # http://localhost:3000/api/products?search=search_text - url example
+    @products = Product.all
+
+    if params[:search]
+      @products = @products.where("name ILIKE?", "%#{params[:search]}%")
+    end
+
+    if params[:sort] == "price" && params[:sort_order] == "desc"
+      @products = @products.order(:price => :desc)
+    end
+
+    if params[:sort] == "price"
+      @products = @products.order(:price => :asc)
+    end
+
+    @products = @products.order(:id => :asc)
+    render "index.json.jb"
+  end
+
   def show
     # http://localhost:3000/api/products/1
     @product = Product.find_by(id: params[:id])
     render "show.json.jb"
-  end
-
-  def index
-    @products = Product.all
-    render "index.json.jb"
   end
 
   def create
@@ -20,7 +35,7 @@ class Api::ProductsController < ApplicationController
     if @product.save
       render "show.json.jb"
     else
-      render json: { errors: @product.errors.full_message }
+      render json: { errors: @product.errors.full_messages }, status: :bad_request
     end
   end
 
@@ -33,9 +48,12 @@ class Api::ProductsController < ApplicationController
     @product.price = params[:price] || @product.price
     @product.image_url = params[:image_url] || @product.image_url
     @product.description = params[:description] || @product.description
-    @product.save
 
-    render "show.json.jb"
+    if @product.save
+      render "show.json.jb"
+    else
+      render json: { errors: @product.errors.full_messages }, status: :bad_request
+    end
   end
 
   def destroy
