7c19b27c83e8c1c98e74bd95098731104ff00714
Taylor Dorsett <taylorsunwatedemails@gmail.com>
2020-09-22 12:12:21 -0500

    updated product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,21 +1,44 @@
 class Product < ApplicationRecord
   validates :name, presence: true
-  validates :price, numericality: { greater_than: 0 }
   validates :name, length: { minimum: 2 }
+  validates :price, numericality: { greater_than: 0 }
   validates :description, length: { in: 2..500 }
+
   belongs_to :supplier
+
+  has_many :images
+
   has_many :category_products
   has_many :categories, through: :category_products
 
-  has_many :images
-  has_many :orders, through: :carted_products
   has_many :carted_products
-end
+  has_many :orders, through: :carted_products
 
-def tax
-  price * 0.9
-end
+  scope :title_search, ->(search_terms) { where("name ILIKE ?", "%#{search_terms}%") if search_terms }
+  scope :discounted, ->(check_discount) { where("price < ?", 19) if check_discount }
+  scope :sorted, ->(sort, sort_order) {
+          if sort == "price" && sort_order == "asc"
+            order(price: :asc)
+          elsif sort == "price" && sort_order == "desc"
+            order(price: :desc)
+          else
+            order(id: :asc)
+          end
+        }
 
-def total
-  price + tax
+  def friendly_updated_at
+    updated_at.strftime("%B%e, %Y")
+  end
+
+  def is_discounted?
+    price <= 19
+  end
+
+  def tax
+    price * 0.9
+  end
+
+  def total
+    price + tax
+  end
 end
