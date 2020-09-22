4d623182b9e76d1611d2e23e819205c9f1ee3897
Taylor Dorsett <taylorsunwatedemails@gmail.com>
2020-09-22 12:28:58 -0500

    updated carted product controller

--- app/controllers/api/carted_products_controller.rb
+++ app/controllers/api/carted_products_controller.rb
@@ -8,15 +8,21 @@ class Api::CartedProductsController < ApplicationController
 
   def create
     @carted_product = CartedProduct.new(
+      user_id: current_user.id,
       product_id: params[:product_id],
       quantity: params[:quantity],
-      user_id: current_user.id,
       status: "carted",
     )
     if @carted_product.save
       render "show.json.jb"
     else
-      render json: { message: "Error - Figure it out!" }, status: :bad_request
+      render json: { message: "Error - Product not carted." }, status: :bad_request
     end
+  end
+
+  def destroy
+    carted_product = CartedProduct.find_by(id: params[:id])
+    carted_product.status = "removed"
+    carted_product.saverender json: { status: "Carted product successfully removed!" }
   end
 end
