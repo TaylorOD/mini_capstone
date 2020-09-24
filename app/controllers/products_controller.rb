2b85ef7ee81abba68882206506c13953bb98b766
Taylor Dorsett <taylorsunwatedemails@gmail.com>
2020-09-23 21:00:44 -0500

    add other restful services

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,6 +4,21 @@ class ProductsController < ApplicationController
     render "index.html.erb"
   end
 
+  def new
+    render "new.html.erb"
+  end
+
+  def create
+    @product = Product.new(
+      name: params[:name],
+      price: params[:price],
+      description: params[:description],
+
+    )
+    @product.save
+    render "show.html.erb"
+  end
+
   def show
     @product = Product.find_by(id: params[:id])
     render "show.html.erb"
